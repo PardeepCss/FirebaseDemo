79cb66f9ebe1baee8e62d1df8ea331cf5aa7260c
cssjockey-25 <pardeep.kumar@cssjockey.com>
2018-12-13 10:48:06 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,45 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import firebase from 'react-native-firebase';
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      isAuthenticated: false,
+    };
+  }
+
+  componentDidMount() {
+    firebase.auth().signInAnonymously()
+      .then(() => {
+        this.setState({
+          isAuthenticated: true,
+        });
+      });
+  }
+
+  render() {
+    // If the user has not authenticated
+    if (!this.state.isAuthenticated) {
+      return null;
+    }
+
+    return (
+      <View>
+        <Text>Welcome to my awesome app!</Text>
+      </View>
+    );
+  }
+
+}
+
+export default App;
